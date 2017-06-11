ef2fdef6e1afeb42771e3eb4e7748ffc36cc0ff5
Tyler Petresky <tnpetresky@gmail.com>
2017-06-11 01:15:43 -0400

    Remove some warnings from the application

--- src/rendering/building/Building.js
+++ src/rendering/building/Building.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import './Building.css'
 
 import renderer from '../../modules/renderer'
--- src/rendering/unit/Unit.js
+++ src/rendering/unit/Unit.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import './Unit.css'
 
 import renderer from '../../modules/renderer'
